d6c0d24b5f7ec3a024d57a2595b588dd106893f5
Joao F P Souza <jfpadilha@gmail.com>
2021-05-30 17:01:47 -0300

    #13 - Contando e agrupando COUNT() e GROUP BY

--- codes.sql
+++ codes.sql
@@ -15,6 +15,15 @@ SELECT *
 FROM film
 ORDER BY title asc; -- crescente
 
+-- COUNT  e GROUP BY (contar e agrupar)
+SELECT rental_rate, count(*)
+FROM film
+GROUP BY rental_rate
+ORDER BY special_features;
+
+SELECT *, COUNT(*)
+FROM film
+GROUP BY rental_duration;
 
 
 
