6ae7b0b468c9e09a1f9f20bcc7c15b83c5cc49ee
Joao F P Souza <jfpadilha@gmail.com>
2021-05-30 01:04:35 -0300

    #12 - criado arquivo 'codes.sql' fazendo consultas com ordenação 'order by'

---
+++ codes.sql
@@ -0,0 +1,21 @@
+-- Selec simples
+SELECT * 
+FROM address;
+
+SELECT * 
+FROM film 
+WHERE film_id = 3;
+
+-- ordenação
+SELECT *
+FROM film
+ORDER BY title desc; -- decrescente
+
+SELECT *
+FROM film
+ORDER BY title asc; -- crescente
+
+
+
+
+
